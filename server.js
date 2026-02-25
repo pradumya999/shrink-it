f8cc9be8f1321b6e2ed5278694b5f3fb1e100f31
Pradumya Gupta <grenu6979@gmail.com>
2026-02-25 17:46:55 +0530

    configured cors & add helmet

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const helmet = require("helmet");
 const signUp = require("./server/routes/signup.js");
 const landing = require("./server/routes/landing.js");
 const login = require("./server/routes/login.js");
@@ -17,14 +19,15 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: true,
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(helmet());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", landing);
 app.use("/", user);
--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async function(){
-    mongoose.connect(`${process.env.MONGODB}`);
+    try{
+        await mongoose.connect(`${process.env.MONGODB}`);
+        console.log("MongoDB Connected");
+    } catch(e){
+        console.error("Database connection failed", error.message);
+    }
 };
 
 module.exports = connectDB;
